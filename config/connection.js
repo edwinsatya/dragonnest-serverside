adedf2d194fb84528e8347a57a69600c60fc72b5
edwinsatya <edwinsatyayudistira@gmail.com>
2021-06-24 19:15:36 +0700

    update connection

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,14 @@ const mongouri = process.env.MONGO_URI;
 
 module.exports = () => {
   mongoose.connect(
-    mongouri + `-${process.env.NODE_ENV}`,
+    mongouri,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
     },
-    err => {
+    (err) => {
       if (err) console.log("Failed To Connect DB");
       else console.log(`Connected To DB`);
     }
